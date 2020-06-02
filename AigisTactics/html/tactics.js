ccbdb8e0bb1e3d65b5c2ee4850e5c4509d82aa54
z1022001jp <z1022001jp@gmail.com>
2020-06-02 10:07:16 +0800

    script

--- AigisTactics/html/tactics.js
+++ AigisTactics/html/tactics.js
@@ -4,37 +4,152 @@ let bodyOnload = function () {
     questList.sort((a, b) => { return a.id.localeCompare(b.id); })
     // console.log("bodyOnload");
 
-    let typeSelect = document.getElementsByClassName("missionType")[0];
+    let select = document.getElementById("missionType");
+    select.options.add(new Option("＝＝分類＝＝", ""));
+    select.options.add(new Option("ストーリーミッション", "Story"));
+    select.options.add(new Option("英傑の塔", "Tower"));
+    select.options.add(new Option("曜日ミッション", "Daily"));
 
-    // typeSelect.options.add(new Option("＝＝分類＝＝", ""));
-    typeSelect.options.add(new Option("ストーリーミッション", "Story"));
-    typeSelect.options.add(new Option("英傑の塔", "Tower"));
-    typeSelect.options.add(new Option("曜日ミッション", "Daily"));
+    select.options.add(new Option("緊急ミッション", "Emergency"));
+    select.options.add(new Option("デイリー復刻", "DailyReproduce"));
 
-    typeSelect.options.add(new Option("緊急ミッション", "Emergency"));
-    typeSelect.options.add(new Option("デイリー復刻", "DailyReproduce"));
+    select.options.add(new Option("魔神降臨", "Devil"));
+    select.options.add(new Option("神獣降臨", "Raid"));
 
-    typeSelect.options.add(new Option("魔神降臨", "Devil"));
-    typeSelect.options.add(new Option("神獣降臨", "Raid"));
+    select.options.add(new Option("交流クエスト", "Harlem"));
+    select.options.add(new Option("戦術指南/チャレンジクエスト", "Challenge"));
 
-    typeSelect.options.add(new Option("交流クエスト", "Harlem"));
-    typeSelect.options.add(new Option("戦術指南/チャレンジクエスト", "Challenge"));
+    select.options.add(new Option("ゴールドラッシュ", "Goldrush"));
+    select.options.add(new Option("復刻ミッション", "Reproduce"));
+    select.options.add(new Option("大討伐", "Subjugation"));
+    select.options.add(new Option("特別ミッション", "Special"));
 
-    typeSelect.options.add(new Option("ゴールドラッシュ", "Goldrush"));
-    typeSelect.options.add(new Option("復刻ミッション", "Reproduce"));
-    typeSelect.options.add(new Option("大討伐", "Subjugation"));
-    typeSelect.options.add(new Option("特別ミッション", "Special"));
+    select = document.getElementById("mission");
+    select.options.add(new Option("＝＝ミッション＝＝", ""));
+
+    select = document.getElementById("quest");
+    select.options.add(new Option("＝＝クエスト＝＝", ""));
+
+    // sort database
+    charaData.sort(function compare(aData, bData) {
+
+        if ((aData.sortGroupID < 20) != (bData.sortGroupID < 20)) {
+            if (aData.sortGroupID < 20) return 1;
+            if (bData.sortGroupID < 20) return -1;
+        } else {
+            if ((aData.sortGroupID < 20) && (bData.sortGroupID < 20)) {
+                if (aData.sortGroupID != bData.sortGroupID) return (aData.sortGroupID < bData.sortGroupID) ? -1 : 1;
+            } else {
+                // sort by rare
+                if (aData.rare != bData.rare) return (aData.rare > bData.rare) ? -1 : 1;
+            }
+        }
+
+        // sort by class
+        if (aData.classId != bData.classId) return (aData.classId < bData.classId) ? -1 : 1;
+        // sort by id
+        if (aData.id != bData.id) return (aData.id < bData.id) ? -1 : 1;
+
+        return 0;
+    })
+
+    // import
+    let iconbox = document.getElementById("iconbox");
+    for (let i in charaData) {
+        if (charaData[i] == null) continue;
+
+        // build dom element
+        let icon = document.createElement("img");
+        icon.className = "iconbtn";
+        icon.title = charaData[i].name; // + "," + charaData[i].classId;
+        icon.src = charaData[i].img;
+        icon.width = 50;
+        icon.height = 50;
+        // icon.id = charaData[i].id;
+        icon.alt = charaData[i].id;
+
+        // onclick event
+        icon.addEventListener("click", addIcon, false);
+
+        // append
+        iconbox.appendChild(icon);
+    }
+
+    // set hr
+    for (let i = 0; i < iconbox.childElementCount; ++i) {
+        // get image & id
+        let a = iconbox.children[i];
+        let b = iconbox.children[i + 1];
+        if (!a || !b || a.tagName != "IMG" || b.tagName != "IMG") continue;
+
+        // get icon data
+        let aData = charaData.find(obj => (obj && obj.id == a.alt));
+        let bData = charaData.find(obj => (obj && obj.id == b.alt));
+        if (!aData || !bData) continue;
+
+        // set text
+        let aText = "";
+        let bText = "";
+
+        let textList = ["アイアン", "ブロンズ", "シルバー", "ゴールド", "プラチナ", "ブラック"];
+        aText = aData.rare == 3.5 ? "サファイア" : textList[parseInt(aData.rare)];
+        bText = bData.rare == 3.5 ? "サファイア" : textList[parseInt(bData.rare)];
+
+        textList = { 10: "聖霊", 11: "トークン/NPC", 12: "その他" }
+        if (aData.sortGroupID < 20) { aText = textList[parseInt(aData.sortGroupID)]; }
+        if (bData.sortGroupID < 20) { bText = textList[parseInt(bData.sortGroupID)]; }
+
+        // set hr or not
+        let hr = document.createElement("div");
+        hr.className = "hr";
+        if (i == 0) {
+            hr.innerHTML = `<span>${aText}</span>`;
+            a.parentNode.insertBefore(hr, a);
+        } else if (aText != bText) {
+            hr.innerHTML = `<span>${bText}</span>`;
+            a.parentNode.insertBefore(hr, b);
+        }
+    }
+}
+let iconCount = 0;
+let addIcon = function (event) {
+    // get chara id
+    let id = event.target.alt;
+
+    // get chara data
+    let icon = charaData.find(icon => { return icon.id == id; })
+    if (!icon) console.log("addIcon error", id);
+
+    // gat mapimg
+    let mapimg = document.getElementById("mapimg");
+
+    // set newIcon element
+    let newIcon = document.createElement("img");
+    newIcon.className = "icon";
+    newIcon.title = icon.name; // + "," + icon.classId;
+    newIcon.src = icon.img;
+    newIcon.width = 50;
+    newIcon.height = 50;
+    newIcon.id = "icon" + iconCount;
+
+    newIcon.addEventListener("dragstart", onDragStart, false);
+
+    newIcon.style.left = 30 + parseInt(iconCount % 20) * 25 + "px";
+    newIcon.style.top = 30 + parseInt(iconCount / 20) * 25 + "px";
+
+    mapimg.appendChild(newIcon);
+    iconCount++;
 }
 
 // select options
-function onChange(select) {
-    if (select.className == "missionType") {
+let onChange = function (select) {
+    if (select.id == "missionType") {
         // change type
         let i = select.selectedIndex;
         let value = select.options[i].value;
 
         // get selected
-        let missionSelect = document.getElementsByClassName("mission")[0];
+        let missionSelect = document.getElementById("mission");
         missionSelect.innerText = null;
         let items = [];
         let missionIds = Object.keys(missionNameList);
@@ -115,13 +230,13 @@ function onChange(select) {
             missionSelect.options.add(new Option(missionNameList[item], item));
         }
 
-    } else if (select.className == "mission") {
+    } else if (select.id == "mission") {
         // change mission
         let i = select.selectedIndex;
         let value = select.options[i].value;
 
         // get selected
-        let questSelect = document.getElementsByClassName("quest")[0];
+        let questSelect = document.getElementById("quest");
         questSelect.innerText = null;
         let items = questList.filter((quest) => { return quest.missionId == value; });
 
@@ -138,7 +253,7 @@ function onChange(select) {
             questSelect.options.add(new Option(item.questTitle, item.id));
         }
 
-    } else if (select.className == "quest") {
+    } else if (select.id == "quest") {
         // change quest
         let i = select.selectedIndex;
         let value = select.options[i].value;
@@ -147,16 +262,19 @@ function onChange(select) {
         let quest = questList.find((quest) => { return quest.id == value; });
 
         // clear map image
-        let mapimg = document.getElementsByClassName("mapimg")[0];
-        mapimg.innerHTML = null;
+        let mapimg = document.getElementById("mapimg");
+        mapimg.innerHTML = null; iconCount = 0;
 
         // set range element
+        let radius = parseInt(document.getElementById("rangebox").value) + 40;
         let range = document.createElement("div");
-        range.className = "range";
+        range.id = "range";
         range.style.visibility = "hidden";
-        let radius = document.getElementsByClassName("rangebox")[0].children[0].value;
         range.style.width = (radius * 1.5) + "px";
         range.style.height = (radius * 1.5) + "px";
+        range.className = "range";
+        range.addEventListener("ondrop", onDrop, false);
+        range.addEventListener("ondragover", onDragOver, false);
         mapimg.appendChild(range);
         // range.style.visibility = "visible";
 
@@ -183,31 +301,33 @@ function onChange(select) {
         }
     }
 }
-function onChangeRange(select) {
-    if (select.className != "rangebox") { return; }
-    // change type
-    let radius = select.value;
+let onChangeInput = function (select) {
+    if (select.id == "rangebox") {
+        // change type
+        let radius = parseInt(select.value) + 40;
 
-    // set range element
-    let range = document.getElementsByClassName("range")[0];
-    if (!range) { return; }
+        // set range element
+        let range = document.getElementById("range");
+        if (!range) { return; }
 
-    range.style.width = (radius * 1.5) + "px";
-    range.style.height = (radius * 1.5) + "px";
+        range.style.width = (radius * 1.5) + "px";
+        range.style.height = (radius * 1.5) + "px";
 
-    // check visibility
-    if (range.style.visibility == "hidden") { return; }
+        // check visibility
+        if (range.style.visibility == "hidden") { return; }
 
-    setLocationOpacity(false);
+        setLocationOpacity(false);
+    }
 }
 
 // set locations opacity
-function setLocationOpacity(isHidden) {
+let setLocationOpacity = function (isHidden) {
 
-    let radius = parseInt(document.getElementsByClassName("rangebox")[0].children[0].value);
-    let x0 = parseInt(event.target.style.left);
-    let y0 = parseInt(event.target.style.top);
-    console.log(x0, y0)
+    let range = document.getElementById("range");
+    let radius = parseInt(document.getElementById("rangebox").value);
+    let x0 = parseInt(range.style.left);
+    let y0 = parseInt(range.style.top);
+
     let locals = document.querySelectorAll(".afar, .near");
     for (let i in locals) {
         let local = locals[i];
@@ -218,69 +338,71 @@ function setLocationOpacity(isHidden) {
         } else {
             let x = parseInt(local.style.left);
             let y = parseInt(local.style.top);
-            let distance = 1.332 * Math.sqrt(Math.pow(x - x0, 2) + Math.pow(y - y0, 2));
-
-            if (x == 546 && y == 349) {
-                console.log(x, x0, y, y0, distance, radius)
-            }
+            let distance = Math.sqrt(Math.pow(x - x0, 2) + Math.pow(y - y0, 2)) / 0.75;
 
             if (distance <= radius) {
                 local.style.opacity = "1.0";
-            } else if (distance <= (radius + 40)) {
-                local.style.opacity = "0.5";
+                // } else if (distance <= (radius + 40)) {
+                //     local.style.opacity = "0.4";
             } else {
-                local.style.opacity = "0.2";
+                local.style.opacity = "0.1";
             }
         }
     }
 }
 
 // mapimg method
-function onDragStart(event) {
+let onDragStart = function (event) {
     console.log("dragStart");
 
     // save drag image dom id
     event.dataTransfer.setData("imgId", event.target.id);
+    event.dataTransfer.setData("startX", event.clientX);     // Get the horizontal coordinate
+    event.dataTransfer.setData("startY", event.clientY);     // Get the vertical coordinate
 
     // set map alpha 0.5
-    document.getElementsByClassName("mapimg")[0].title = "alpha";
+    document.getElementById("mapimg").title = "alpha";
+    // document.getElementById("range").hidden = true;
 }
-function onDragOver(event) {
-    // console.log("allowDrop");
+let onDragOver = function (event) {
+    // console.log("ondragover");
     event.preventDefault();
 }
-function onDrop(event) {
+let onDrop = function (event) {
     // console.log("drop");
     event.preventDefault();
 
     // get mapimg
-    let mapimg = document.getElementsByClassName("mapimg")[0];
+    let mapimg = document.getElementById("mapimg");
 
     // set map alpha 0.0
     mapimg.title = "";
+    // document.getElementById("range").outerHTML = document.getElementById("range").outerHTML.replace(" hidden", " ");
 
     // get drop area
     let target = event.target;
     let className = target.className;
 
     // get drag image
-    let imgId = event.dataTransfer.getData("imgId");
-    let img = document.getElementById(imgId);
+    let img = document.getElementById(event.dataTransfer.getData("imgId"));
+    let startX = event.dataTransfer.getData("startX");
+    let startY = event.dataTransfer.getData("startY");
+    let endX = event.clientX;
+    let endY = event.clientY;
 
     if (className == "goal") {
         mapimg.removeChild(img);
     } else if (className == "near" || className == "afar") {
         img.style.left = target.style.left;
         img.style.top = target.style.top;
-    } else if (className == "mapimg") {
-        console.log(event)
-        img.style.left = event.offsetX + "px";
-        img.style.top = event.offsetY + "px";
+    } else if (className == "mapimg" || className == "range") {
+        img.style.left = parseInt(img.style.left) + endX - startX + "px";
+        img.style.top = parseInt(img.style.top) + endY - startY + "px";
     }
 }
-function onClick(event) {
+let onClick = function (event) {
     // set range element
-    let range = document.getElementsByClassName("range")[0];
+    let range = document.getElementById("range");
     if (!range) { return; }
 
     // check posion
