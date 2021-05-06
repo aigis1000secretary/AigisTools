33d10ae0b1f652edb8e339267a4b4e02408e061d
K.T.(Mirror-VPC) <z1022001@gmail.com>
2021-05-06 20:29:19 +0800

    update0506

--- html/script/rawCardsList.js
+++ html/script/rawCardsList.js
@@ -1,4 +1,4 @@
-var maxCid = 1203;
+var maxCid = 1205;
 var charaData = [
 	{	"id": 1,	"name": "主人公",	"rare": 5,	"classID": 9900,	"sortGroupID": 25,	"placeType": 1,	"kind": 0,	"assign": 0,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "001_00"	},
 	{	"id": 2,	"name": "一般兵A【弓】",	"rare": 0,	"classID": 10001,	"sortGroupID": 30,	"placeType": 2,	"kind": 0,	"assign": 0,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "002_00"	},
@@ -1202,5 +1202,7 @@ var charaData = [
 	{	"id": 1200,	"name": "ウェパル人形",	"rare": 5,	"classID": 50,	"sortGroupID": 10,	"placeType": 0,	"kind": 3,	"assign": 0,	"genus": 0,	"year": 2021,	"isEvent": 1,	"isToken": 0,	"img": "1200_00"	},
 	{	"id": 1201,	"name": "等身大ケラウノス人形",	"rare": 5,	"classID": 51,	"sortGroupID": 10,	"placeType": 0,	"kind": 3,	"assign": 0,	"genus": 0,	"year": 2021,	"isEvent": 1,	"isToken": 0,	"img": "1201_00"	},
 	{	"id": 1202,	"name": "銀腕を掲げし者トラム",	"rare": 11,	"classID": 127400,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 0,	"year": 2021,	"isEvent": 1,	"isToken": 1,	"img": "1202_00"	},
-	{	"id": 1203,	"name": "ターゲットマーカー",	"rare": 5,	"classID": 121820,	"sortGroupID": 20,	"placeType": 1,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2021,	"isEvent": 1,	"isToken": 1,	"img": "1203_00"	}
+	{	"id": 1203,	"name": "ターゲットマーカー",	"rare": 5,	"classID": 121820,	"sortGroupID": 20,	"placeType": 1,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2021,	"isEvent": 1,	"isToken": 1,	"img": "1203_00"	},
+	{	"id": 1204,	"name": "ちび鬼刃姫",	"rare": 3,	"classID": 24700,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 108,	"year": 2021,	"isEvent": 1,	"isToken": 0,	"img": "1204_00"	},
+	{	"id": 1205,	"name": "ちびリンネ",	"rare": 3,	"classID": 31200,	"sortGroupID": 30,	"placeType": 2,	"kind": 1,	"assign": 0,	"genus": 108,	"year": 2021,	"isEvent": 1,	"isToken": 0,	"img": "1205_00"	}
 ]
