bae44d5c2417a559cbe9ff76e40714091656c545
K.T.(HUA-VPC) <z1022001jp@gmail.com>
2021-09-28 09:25:45 +0800

    del EXP.html

--- html/EXP/toggleLanguage.js
+++
@@ -1,9 +0,0 @@
-var classList = document.body.getElementsByClassName("lang");
-
-function toggleLanguage(){
-  for(var i=0;i<classList.length;i++){
-    var temp = classList[i].innerHTML;
-    classList[i].innerHTML = classList[i].getAttribute("inner");
-    classList[i].setAttribute("inner",temp);
-  }
-}
