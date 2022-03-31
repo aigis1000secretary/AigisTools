080300f53aaa58e167412ca454aeb2178bf9042f
K.T <z1022001@gmail.com>
2022-03-31 16:39:51 +0800

    update0331

--- html/script/rawCardsList.js
+++ html/script/rawCardsList.js
@@ -1,4 +1,4 @@
-var maxCid = 1447;
+var maxCid = 1453;
 var charaData = [
 	{	"id": 1,	"name": "主人公",	"rare": 5,	"classID": 9900,	"sortGroupID": 25,	"placeType": 1,	"kind": 0,	"assign": 0,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "001_00"	},
 	{	"id": 2,	"name": "一般兵A【弓】",	"rare": 0,	"classID": 10001,	"sortGroupID": 30,	"placeType": 2,	"kind": 0,	"assign": 0,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "002_00"	},
@@ -1443,8 +1443,14 @@ var charaData = [
 	{	"id": 1441,	"name": "暗黒学園騎士コーネリア",	"rare": 5,	"classID": 1200,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 103,	"year": 2022,	"isEvent": 0,	"isToken": 0,	"img": "1441_00",	"imgaw": "1441_01"	},
 	{	"id": 1442,	"name": "戦術科教師ケイティ",	"rare": 4,	"classID": 100,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 103,	"year": 2022,	"isEvent": 0,	"isToken": 0,	"img": "1442_00",	"imgaw": "1442_01"	},
 	{	"id": 1443,	"name": "山賊食堂モーティマ",	"rare": 4,	"classID": 600,	"sortGroupID": 20,	"placeType": 1,	"kind": 0,	"assign": 0,	"genus": 103,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1443_00",	"imgaw": "1443_01"	},
-	{	"id": 1444,	"name": "鋼鉄の弓術部員クレア",	"rare": 4,	"classID": 10000,	"sortGroupID": 30,	"placeType": 2,	"kind": 1,	"assign": 0,	"genus": 103,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1444_00"	},
+	{	"id": 1444,	"name": "鋼鉄の弓術部員クレア",	"rare": 4,	"classID": 10000,	"sortGroupID": 30,	"placeType": 2,	"kind": 1,	"assign": 0,	"genus": 103,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1444_00",	"imgaw": "1444_01"	},
 	{	"id": 1445,	"name": "王子【焔竜】",	"rare": 5,	"classID": 131800,	"sortGroupID": 25,	"placeType": 1,	"kind": 0,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1445_00"	},
 	{	"id": 1446,	"name": "セツナ（分身）",	"rare": 5,	"classID": 121470,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1446_00"	},
-	{	"id": 1447,	"name": "セツナ（分身）",	"rare": 5,	"classID": 121480,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1447_00"	}
+	{	"id": 1447,	"name": "セツナ（分身）",	"rare": 5,	"classID": 121480,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1447_00"	},
+	{	"id": 1448,	"name": "かまいたち群青姉妹",	"rare": 5,	"classID": 126500,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 0,	"year": 2022,	"isEvent": 0,	"isToken": 0,	"img": "1448_00",	"imgaw": "1448_01"	},
+	{	"id": 1449,	"name": "かまいたちトキ",	"rare": 5,	"classID": 126600,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1449_00"	},
+	{	"id": 1450,	"name": "疾風かまいたちトキ",	"rare": 5,	"classID": 126630,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1450_00"	},
+	{	"id": 1451,	"name": "かまいたちユキ",	"rare": 5,	"classID": 126700,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1451_00"	},
+	{	"id": 1452,	"name": "疾風かまいたちユキ",	"rare": 5,	"classID": 126730,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1452_00"	},
+	{	"id": 1453,	"name": "超古代発明家アルレット",	"rare": 5,	"classID": 16600,	"sortGroupID": 30,	"placeType": 2,	"kind": 1,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1453_00",	"imgaw": "1453_01"	}
 ]
