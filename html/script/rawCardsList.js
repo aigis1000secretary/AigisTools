4f109863929b29d6e4834ef59e25428c99843971
K.T <z1022001@gmail.com>
2022-04-07 14:16:36 +0800

    update0407

--- html/script/rawCardsList.js
+++ html/script/rawCardsList.js
@@ -1,4 +1,4 @@
-var maxCid = 1453;
+var maxCid = 1456;
 var charaData = [
 	{	"id": 1,	"name": "主人公",	"rare": 5,	"classID": 9900,	"sortGroupID": 25,	"placeType": 1,	"kind": 0,	"assign": 0,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "001_00"	},
 	{	"id": 2,	"name": "一般兵A【弓】",	"rare": 0,	"classID": 10001,	"sortGroupID": 30,	"placeType": 2,	"kind": 0,	"assign": 0,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "002_00"	},
@@ -1452,5 +1452,8 @@ var charaData = [
 	{	"id": 1450,	"name": "疾風かまいたちトキ",	"rare": 5,	"classID": 126630,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1450_00"	},
 	{	"id": 1451,	"name": "かまいたちユキ",	"rare": 5,	"classID": 126700,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1451_00"	},
 	{	"id": 1452,	"name": "疾風かまいたちユキ",	"rare": 5,	"classID": 126730,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1452_00"	},
-	{	"id": 1453,	"name": "超古代発明家アルレット",	"rare": 5,	"classID": 16600,	"sortGroupID": 30,	"placeType": 2,	"kind": 1,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1453_00",	"imgaw": "1453_01"	}
+	{	"id": 1453,	"name": "超古代発明家アルレット",	"rare": 5,	"classID": 16600,	"sortGroupID": 30,	"placeType": 2,	"kind": 1,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1453_00",	"imgaw": "1453_01"	},
+	{	"id": 1454,	"name": "紫炎の鬼傭兵アクラ",	"rare": 4,	"classID": 4700,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 8,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1454_00",	"imgaw": "1454_01"	},
+	{	"id": 1455,	"name": "ちびアナトリア",	"rare": 3,	"classID": 25300,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 108,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1455_00"	},
+	{	"id": 1456,	"name": "ちび竜吉公主",	"rare": 3,	"classID": 27300,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 108,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1456_00"	}
 ]
