9a15ff091367fffc95f95e5d16c44c70f1802571
K.T <z1022001@gmail.com>
2022-09-08 16:23:21 +0800

    update0908

--- html/script/rawCardsList.js
+++ html/script/rawCardsList.js
@@ -1,4 +1,4 @@
-var maxCid = 1565;
+var maxCid = 1566;
 var charaData = [
 	{	"id": 1,	"name": "主人公",	"rare": 5,	"classID": 9900,	"sortGroupID": 25,	"placeType": 1,	"kind": 0,	"assign": 0,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "001_00"	},
 	{	"id": 2,	"name": "一般兵A【弓】",	"rare": 0,	"classID": 10001,	"sortGroupID": 30,	"placeType": 2,	"kind": 0,	"assign": 0,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "002_00"	},
@@ -1564,5 +1564,6 @@ var charaData = [
 	{	"id": 1562,	"name": "ビーストメイジ",	"rare": 5,	"classID": 152300,	"sortGroupID": 30,	"placeType": 2,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1562_00"	},
 	{	"id": 1563,	"name": "ちびイザムバード",	"rare": 3,	"classID": 225700,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 108,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1563_00"	},
 	{	"id": 1564,	"name": "ちびルヴェア",	"rare": 3,	"classID": 21500,	"sortGroupID": 20,	"placeType": 1,	"kind": 1,	"assign": 0,	"genus": 108,	"year": 2022,	"isEvent": 1,	"isToken": 0,	"img": "1564_00"	},
-	{	"id": 1565,	"name": "スカイヒーラートークン",	"rare": 5,	"classID": 17000,	"sortGroupID": 20,	"placeType": 3,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1565_00"	}
+	{	"id": 1565,	"name": "スカイヒーラートークン",	"rare": 5,	"classID": 17000,	"sortGroupID": 20,	"placeType": 3,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 1,	"isToken": 1,	"img": "1565_00"	},
+	{	"id": 1566,	"name": "風竜魔術士コラリー",	"rare": 4,	"classID": 150600,	"sortGroupID": 30,	"placeType": 2,	"kind": 1,	"assign": 0,	"genus": 0,	"year": 2022,	"isEvent": 0,	"isToken": 0,	"img": "1566_00",	"imgaw": "1566_01"	}
 ]
