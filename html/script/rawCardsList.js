195aa9f8766119e289ee44c9396ff614ee2c3cd1
Yinan <z1022001@gmail.com>
2026-02-12 21:57:22 +0800

    update0212

--- html/script/rawCardsList.js
+++ html/script/rawCardsList.js
@@ -1,4 +1,4 @@
-var maxCid = 2712;
+var maxCid = 2715;
 var charaData = [
 	{	"id": 1,	"name": "主人公",	"rare": 5,	"classID": 9900,	"sortGroupID": 25,	"placeType": 1,	"kind": 0,	"assign": 1,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "001_00"	},
 	{	"id": 2,	"name": "一般兵A【弓】",	"rare": 0,	"classID": 10001,	"sortGroupID": 30,	"placeType": 2,	"kind": 0,	"assign": 1,	"genus": 0,	"year": 2013,	"isEvent": 1,	"isToken": 0,	"img": "002_00"	},
@@ -2446,5 +2446,8 @@ var charaData = [
 	{	"id": 2707,	"name": "ナーガ様のゴーレム",	"rare": 5,	"classID": 300600,	"sortGroupID": 30,	"placeType": 1,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2026,	"isEvent": 1,	"isToken": 1,	"img": "c80ae4db8b6b09123493ceea8b63ccc2"	},
 	{	"id": 2708,	"name": "ドラゴンゴーレム",	"rare": 5,	"classID": 300640,	"sortGroupID": 30,	"placeType": 1,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2026,	"isEvent": 1,	"isToken": 1,	"img": "c80ae4db8b6b09123493ceea8b63ccc2"	},
 	{	"id": 2709,	"name": "泥岩人形",	"rare": 5,	"classID": 300700,	"sortGroupID": 30,	"placeType": 1,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2026,	"isEvent": 1,	"isToken": 1,	"img": "c80ae4db8b6b09123493ceea8b63ccc2"	},
-	{	"id": 2710,	"name": "泥岩竜",	"rare": 5,	"classID": 300740,	"sortGroupID": 30,	"placeType": 1,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2026,	"isEvent": 1,	"isToken": 1,	"img": "c80ae4db8b6b09123493ceea8b63ccc2"	}
+	{	"id": 2710,	"name": "泥岩竜",	"rare": 5,	"classID": 300740,	"sortGroupID": 30,	"placeType": 1,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2026,	"isEvent": 1,	"isToken": 1,	"img": "c80ae4db8b6b09123493ceea8b63ccc2"	},
+	{	"id": 2713,	"name": "時を手繰る者マイズ",	"rare": 10,	"classID": 166100,	"sortGroupID": 30,	"placeType": 2,	"kind": 1,	"assign": 0,	"genus": 0,	"year": 2026,	"isEvent": 0,	"isToken": 0,	"img": "c80ae4db8b6b09123493ceea8b63ccc2"	},
+	{	"id": 2714,	"name": "ちび迅天君",	"rare": 3,	"classID": 250100,	"sortGroupID": 20,	"placeType": 1,	"kind": 0,	"assign": 9,	"genus": 108,	"year": 2026,	"isEvent": 1,	"isToken": 0,	"img": "2714_00"	},
+	{	"id": 2715,	"name": "ちび迅天君移動用トークン",	"rare": 3,	"classID": 166400,	"sortGroupID": 20,	"placeType": 1,	"kind": 2,	"assign": 0,	"genus": 0,	"year": 2026,	"isEvent": 1,	"isToken": 1,	"img": "2715_00"	}
 ]
